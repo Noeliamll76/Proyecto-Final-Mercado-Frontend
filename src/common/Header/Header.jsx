f08146ef8100df906deac47bdfe5b2923a613d8e
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-12-13 10:53:40 +0100

    Feat: Include the logout in the header

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -2,28 +2,38 @@
 import React from 'react'
 import './Header.css'
 import { LinkButton } from '../LinkButton/LinkButton'
+import { useSelector, useDispatch } from "react-redux";
+import { logout, userData } from "../../pages/userSlice";
+import { useNavigate } from 'react-router-dom';
 
 export const Header = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
+    const rdxCredentialsUser = useSelector(userData);
+
+    const logOutMe = () => {
+        dispatch(logout({ credentials: "" }))
+        navigate("/")
+    }
 
     return (
-        <div className='headerDesign'>
-            <LinkButton
-                path={"/"}
-                title={"Home"}
-            />
-            <LinkButton
-                path={"/register"}
-                title={"Register"}
-            />
-            <LinkButton
-                path={"/login"}
-                title={"Login"}
-            />
-            <LinkButton
-                path={"/profile"}
-                title={"Profile"}
-            />
+        <div className="headerDesign">
+            <LinkButton path={"/"} title={"Home"} />
+            {!rdxCredentialsUser?.credentials.token ? (
+                <>
+                    <LinkButton path={"/register"} title={"Register"} />
+                    <LinkButton path={"/login"} title={"Login"} />
+                </>
+            ) : (
+                <>
+                    <LinkButton path={"/profile"} title={"Profile"} />
+
+                    <div onClick={logOutMe}>
+                        <LinkButton path={"/"} title={"Log out"} />
+                    </div>
+                </>
+            )}
         </div>
     )
 }
--- src/pages/Update/Update.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import './Update.css'
-export const Update = () => {
-     return (
-         <div className="UpdateDesign">UPDATE</div>
-     )
-}
