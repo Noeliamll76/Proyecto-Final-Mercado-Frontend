2add584bc018236e378f02241579e05818fe9afd
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-12-28 12:48:03 +0100

    Feat: Add logo-mercat into header

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -4,9 +4,9 @@ import './Header.css'
 import { LinkButton } from '../LinkButton/LinkButton'
 import { useSelector, useDispatch } from "react-redux";
 import { logout, userData } from "../../pages/userSlice";
-import { basketData } from "../../pages/basketSlice";
+import { logoutBasket, basketData } from "../../pages/basketSlice";
 import { useNavigate } from 'react-router-dom';
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import logoMercat from '../../../public/image/LOGO-MERCAT.png'
 
 export const Header = () => {
     const navigate = useNavigate();
@@ -14,33 +14,43 @@ export const Header = () => {
 
     const rdxCredentialsUser = useSelector(userData);
     const rdxInfoBasket = useSelector(basketData);
-    
+
     const logOutMe = () => {
         dispatch(logout({ credentials: "" }))
+        dispatch(logoutBasket({ infoBasket: "" }))
         navigate("/")
     }
 
     return (
-        <div className="headerDesign">
-            <LinkButton path={"/"} title={"Home"} />
-            {!rdxCredentialsUser?.credentials.token ? (
-                <>
-                    <LinkButton path={"/register"} title={"Register"} />
-                    <LinkButton path={"/login"} title={"Login"} />
-                </>
-            ) : (
-                <>
-                    <LinkButton path={"/profile"} title={"Profile"} />
-
-                    <div onClick={logOutMe}>
-                        <LinkButton path={"/"} title={"Log out"} />
+        <>
+            <div className="headerDesign">
+                <div className='headerBox'>
+                    <div className="headerLogo">
+                        <div><img className="logoDesign" src={logoMercat} /></div>
                     </div>
-                    {!rdxInfoBasket?.info
-                        ? (<LinkButton path={"/profileBasket"} title={"CESTA"} />)
-                        : ('') }
 
-                </>
-            )}
-        </div>
+                    <div className="headerButtons">
+                        <LinkButton path={"/"} title={"Home"} />
+                        {!rdxCredentialsUser?.credentials.token ? (
+                            <>
+                                <LinkButton path={"/register"} title={"Register"} />
+                                <LinkButton path={"/login"} title={"Login"} />
+                            </>
+                        ) : (
+                            <>
+                                <LinkButton path={"/profile"} title={"Profile"} />
+
+                                <div onClick={logOutMe}>
+                                    <LinkButton path={"/"} title={"Log out"} />
+                                </div>
+                                {!rdxInfoBasket?.info
+                                    ? (<LinkButton path={"/profileBasket"} title={"CESTA"} />)
+                                    : ('')}
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div >
+        </>
     )
 }
