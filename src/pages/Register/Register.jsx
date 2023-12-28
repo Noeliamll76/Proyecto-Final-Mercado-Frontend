0c7ca9b5dbd6ec49f596a341f46d29061108c12c
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-12-28 23:31:08 +0100

    feat: improve view register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -80,119 +80,114 @@ export const Register = () => {
 
     return (
         <div className="registerDesign">
-            
-            <div>Nombre :
-                <CustomInput
-                    design={`inputDesign ${userError.nameError !== "" ? 'inputDesignError' : ''}`}
-                    type={"text"}
-                    name={"name"}
-                    placeholder={"Name and surname"}
-                    functionProp={functionHandler}
-                    functionBlur={errorCheck}
-                />
-                <div className='errorMsg'>{userError.nameError}</div>
-            </div>
-      
-            <div>Dirección :
-                <CustomInput
-                    design={`inputDesign ${userError.addressError !== "" ? 'inputDesignError' : ''}`}
-                    type={"text"}
-                    name={"address"}
-                    placeholder={"Calle, número y puerta"}
-                    functionProp={functionHandler}
-                    functionBlur={errorCheck}
-                />
-                <div className='errorMsg'>{userError.addressError}</div>
-            </div>
 
-            <div>Código Postal :
-                <CustomInput
-                    design={`inputDesign ${userError.zip_codeError !== "" ? 'inputDesignError' : ''}`}
-                    type={"number"}
-                    name={"zip_code"}
-                    placeholder={"46000"}
-                    functionProp={functionHandler}
-                    functionBlur={errorCheck}
-                />
-                <div className='errorMsg'>{userError.zip_codeError}</div>
+            <div className="containerRegisterDesign">
+                <div className='containerTitle'>
+                    <div>Nombre : </div>
+                    <div>Dirección : </div>
+                    <div>Código Postal : </div>
+                    <div>Población : </div>
+                    <div>Phone number : </div>
+                    <div>DNI : </div>
+                    <div>Fecha de nacimiento : </div>
+                    <div>Email de usuario : </div>
+                    <div>Password : </div>
+                </div>
+
+                <div className='containerText'>
+                    <CustomInput
+                        design={`inputDesign ${userError.nameError !== "" ? 'inputDesignError' : ''}`}
+                        type={"text"}
+                        name={"name"}
+                        placeholder={"Name and surname"}
+                        functionProp={functionHandler}
+                        functionBlur={errorCheck}
+                    />
+                    <div className='errorMsg'>{userError.nameError}</div>
+
+                    <CustomInput
+                        design={`inputDesign ${userError.addressError !== "" ? 'inputDesignError' : ''}`}
+                        type={"text"}
+                        name={"address"}
+                        placeholder={"Calle, número y puerta"}
+                        functionProp={functionHandler}
+                        functionBlur={errorCheck}
+                    />
+                    <div className='errorMsg'>{userError.addressError}</div>
+
+                    <CustomInput
+                        design={`inputDesign ${userError.zip_codeError !== "" ? 'inputDesignError' : ''}`}
+                        type={"number"}
+                        name={"zip_code"}
+                        placeholder={"46000"}
+                        functionProp={functionHandler}
+                        functionBlur={errorCheck}
+                    />
+                    <div className='errorMsg'>{userError.zip_codeError}</div>
+
+                    <CustomInput
+                        design={`inputDesign ${userError.townError !== "" ? 'inputDesignError' : ''}`}
+                        type={"text"}
+                        name={"town"}
+                        placeholder={"Valencia"}
+                        functionProp={functionHandler}
+                        functionBlur={errorCheck}
+                    />
+                    <div className='errorMsg'>{userError.townError}</div>
+
+                    <CustomInput
+                        design={`inputDesign ${userError.phoneError !== "" ? 'inputDesignError' : ''}`}
+                        type={"text"}
+                        name={"phone"}
+                        placeholder={"phone"}
+                        functionProp={functionHandler}
+                        functionBlur={errorCheck}
+                    />
+                    <div className='errorMsg'>{userError.phoneError}</div>
+
+                    <CustomInput
+                        design={`inputDesign ${userError.DNIError !== "" ? 'inputDesignError' : ''}`}
+                        type={"text"}
+                        name={"DNI"}
+                        placeholder={"00000000X"}
+                        functionProp={functionHandler}
+                        functionBlur={errorCheck}
+                    />
+                    <div className='errorMsg'>{userError.DNIError}</div>
+
+                    <CustomInput
+                        design={`inputDesign ${userError.birthdateError !== "" ? 'inputDesignError' : ''}`}
+                        type={"date"}
+                        name={"birthdate"}
+                        functionProp={functionHandler}
+                        functionBlur={errorCheck}
+                    />
+                    <div className='errorMsg'>{userError.birthdateError}</div>
+
+                    <CustomInput
+                        design={`inputDesign ${userError.emailError !== "" ? 'inputDesignError' : ''}`}
+                        type={"email"}
+                        name={"email"}
+                        placeholder={"Email"}
+                        functionProp={functionHandler}
+                        functionBlur={errorCheck}
+                    />
+                    <div className='errorMsg'>{userError.emailError}</div>
+
+                    <CustomInput
+                        design={`inputDesign ${userError.passwordError !== "" ? 'inputDesignError' : ''}`}
+                        type={"password"}
+                        name={"password"}
+                        placeholder={"password 8-10 characters"}
+                        functionProp={functionHandler}
+                        functionBlur={errorCheck}
+                    />
+                    <div className='errorMsg'>{userError.passwordError}</div>
+                </div>
+
+                <div className='errorMsg'>{msgError}</div>
             </div>
-
-            <div>Población :
-                <CustomInput
-                    design={`inputDesign ${userError.townError !== "" ? 'inputDesignError' : ''}`}
-                    type={"text"}
-                    name={"town"}
-                    placeholder={"Valencia"}
-                    functionProp={functionHandler}
-                    functionBlur={errorCheck}
-                />
-                <div className='errorMsg'>{userError.townError}</div>
-            </div>
-       
-
-            <div>Phone number :
-                <CustomInput
-                    design={`inputDesign ${userError.phoneError !== "" ? 'inputDesignError' : ''}`}
-                    type={"text"}
-                    name={"phone"}
-                    placeholder={"phone"}
-                    functionProp={functionHandler}
-                    functionBlur={errorCheck}
-                />
-                <div className='errorMsg'>{userError.phoneError}</div>
-            </div>
-
-            <div>DNI :
-                <CustomInput
-                    design={`inputDesign ${userError.DNIError !== "" ? 'inputDesignError' : ''}`}
-                    type={"text"}
-                    name={"DNI"}
-                    placeholder={"00000000X"}
-                    functionProp={functionHandler}
-                    functionBlur={errorCheck}
-                />
-                <div className='errorMsg'>{userError.DNIError}</div>
-            </div>
-
-            <div>Fecha de nacimiento :
-                <CustomInput
-                    design={`inputDesign ${userError.birthdateError !== "" ? 'inputDesignError' : ''}`}
-                    type={"date"}
-                    name={"birthdate"}
-                    functionProp={functionHandler}
-                    functionBlur={errorCheck}
-                />
-                <div className='errorMsg'>{userError.birthdateError}</div>
-            </div>
-            
-            <div>Email de usuario :
-                <CustomInput
-                    design={`inputDesign ${userError.emailError !== "" ? 'inputDesignError' : ''}`}
-                    type={"email"}
-                    name={"email"}
-                    placeholder={"Email"}
-                    functionProp={functionHandler}
-                    functionBlur={errorCheck}
-                />
-                <div className='errorMsg'>{userError.emailError}</div>
-            </div>
-
-            <div>Password :
-                <CustomInput
-                    design={`inputDesign ${userError.passwordError !== "" ? 'inputDesignError' : ''}`}
-                    type={"password"}
-                    name={"password"}
-                    placeholder={"password 8-10 characters"}
-                    functionProp={functionHandler}
-                    functionBlur={errorCheck}
-                />
-                <div className='errorMsg'>{userError.passwordError}</div>
-            </div>
-
-
-            <div className='errorMsg'>{msgError}</div>
-
-            <div className='buttonSubmit' onClick={Submit}>Check in?</div>
+                <div className='buttonSubmit' onClick={Submit}>Check </div>
         </div>
     )
 }
